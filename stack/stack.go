42cf19278fa7b7088fee232ef8e2f8ab153d7f40
Diego Giagio <diego@giagio.com>
2014-12-24 11:07:11 -0200

    Initial stack implementation

---
+++ stack/stack.go
@@ -0,0 +1,62 @@
+// Copyright 2014 Diego Giagio <diego@giagio.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package stack
+
+type Stack struct {
+	arr []interface{}
+	n   int
+}
+
+func New() *Stack {
+	return &Stack{arr: nil, n: 0}
+}
+
+func (s *Stack) Push(item interface{}) {
+	// Grow capacity if needed
+	if s.n == cap(s.arr) {
+		s.resize((s.n + 1) * 2)
+	}
+
+	s.arr[s.n] = item
+	s.n++
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.n <= 0 {
+		return nil
+	}
+
+	s.n--
+	item := s.arr[s.n]
+	s.arr[s.n] = nil
+
+	// Shrink if capacity is too big
+	if s.n > 0 && s.n <= cap(s.arr)/4 {
+		s.resize((cap(s.arr) - 1) / 2)
+	}
+
+	return item
+}
+
+func (s *Stack) Peek() interface{} {
+	if s.n <= 0 {
+		return nil
+	}
+	return s.arr[s.n-1]
+}
+
+func (s *Stack) Len() int {
+	return s.n
+}
+
+func (s *Stack) Cap() int {
+	return cap(s.arr)
+}
+
+func (s *Stack) resize(n int) {
+	newArr := make([]interface{}, n)
+	copy(newArr, s.arr)
+	s.arr = newArr
+}
---
+++ stack/stack_test.go
@@ -0,0 +1,88 @@
+package stack
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	s := New()
+	verifyLenAndCap(t, s, 0, 0)
+
+	s.Push(1)
+	verifyLenAndCap(t, s, 1, 2)
+
+	n := s.Peek()
+	verifyValue(t, n, 1)
+	verifyLenAndCap(t, s, 1, 2)
+
+	n = s.Pop()
+	verifyValue(t, n, 1)
+	verifyLenAndCap(t, s, 0, 2)
+
+	n = s.Pop()
+	verifyValue(t, n, nil)
+	verifyLenAndCap(t, s, 0, 2)
+
+	s.Push(9)
+	verifyLenAndCap(t, s, 1, 2)
+	s.Push(8)
+	verifyLenAndCap(t, s, 2, 2)
+	s.Push(7)
+	verifyLenAndCap(t, s, 3, 6)
+	s.Push(6)
+	verifyLenAndCap(t, s, 4, 6)
+	s.Push(5)
+	verifyLenAndCap(t, s, 5, 6)
+	s.Push(4)
+	verifyLenAndCap(t, s, 6, 6)
+	s.Push(3)
+	verifyLenAndCap(t, s, 7, 14)
+	s.Push(2)
+	verifyLenAndCap(t, s, 8, 14)
+	s.Push(1)
+	verifyLenAndCap(t, s, 9, 14)
+
+	n = s.Pop()
+	verifyValue(t, n, 1)
+	verifyLenAndCap(t, s, 8, 14)
+	n = s.Pop()
+	verifyValue(t, n, 2)
+	verifyLenAndCap(t, s, 7, 14)
+	n = s.Pop()
+	verifyValue(t, n, 3)
+	verifyLenAndCap(t, s, 6, 14)
+	n = s.Pop()
+	verifyValue(t, n, 4)
+	verifyLenAndCap(t, s, 5, 14)
+	n = s.Pop()
+	verifyValue(t, n, 5)
+	verifyLenAndCap(t, s, 4, 14)
+	n = s.Pop()
+	verifyValue(t, n, 6)
+	verifyLenAndCap(t, s, 3, 6)
+	n = s.Pop()
+	verifyValue(t, n, 7)
+	verifyLenAndCap(t, s, 2, 6)
+	n = s.Pop()
+	verifyValue(t, n, 8)
+	verifyLenAndCap(t, s, 1, 2)
+	n = s.Pop()
+	verifyValue(t, n, 9)
+	verifyLenAndCap(t, s, 0, 2)
+	n = s.Pop()
+	verifyValue(t, n, nil)
+	verifyLenAndCap(t, s, 0, 2)
+}
+
+func verifyLenAndCap(t *testing.T, s *Stack, l, c int) {
+	if s.Len() != l {
+		t.Errorf("Len must be %d", l)
+	}
+	if s.Cap() != c {
+		t.Errorf("Cap must be %d", c)
+	}
+}
+
+func verifyValue(t *testing.T, n, expected interface{}) {
+	if n != expected {
+		t.Errorf("Value incorrect: %d, expected: %d", n, expected)
+	}
+}
